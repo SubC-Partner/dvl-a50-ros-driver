9ede25982cc12089209a4cc003b771fb4458256c
Christian Mai <chrimai@energy.aau.dk>
2021-10-25 15:46:07 +0200

    Modified publisher.py to fit with updated DVL firmware JSON output V2

--- scripts/publisher.py
+++ scripts/publisher.py
@@ -65,47 +65,48 @@ def publisher():
 		
 		theDVL.header.stamp = rospy.Time.now()
 		theDVL.header.frame_id = "dvl_link"
-		theDVL.time = data["time"]
-		theDVL.velocity.x = data["vx"]
-		theDVL.velocity.y = data["vy"]
-		theDVL.velocity.z = data["vz"]
-		theDVL.fom = data["fom"]
-		theDVL.altitude = data["altitude"]
-		theDVL.velocity_valid = data["velocity_valid"]
-		theDVL.status = data["status"]
-		theDVL.form = data["format"]
+		if "time" in data:
+			theDVL.time = data["time"]
+			theDVL.velocity.x = data["vx"]
+			theDVL.velocity.y = data["vy"]
+			theDVL.velocity.z = data["vz"]
+			theDVL.fom = data["fom"]
+			theDVL.altitude = data["altitude"]
+			theDVL.velocity_valid = data["velocity_valid"]
+			theDVL.status = data["status"]
+			theDVL.form = data["format"]
 		
-		beam0.id = data["transducers"][0]["id"]
-		beam0.velocity = data["transducers"][0]["velocity"]
-		beam0.distance = data["transducers"][0]["distance"]
-		beam0.rssi = data["transducers"][0]["rssi"]
-		beam0.nsd = data["transducers"][0]["nsd"]
-		beam0.valid = data["transducers"][0]["beam_valid"]
+			beam0.id = data["transducers"][0]["id"]
+			beam0.velocity = data["transducers"][0]["velocity"]
+			beam0.distance = data["transducers"][0]["distance"]
+			beam0.rssi = data["transducers"][0]["rssi"]
+			beam0.nsd = data["transducers"][0]["nsd"]
+			beam0.valid = data["transducers"][0]["beam_valid"]
 		
-		beam1.id = data["transducers"][1]["id"]
-		beam1.velocity = data["transducers"][1]["velocity"]
-		beam1.distance = data["transducers"][1]["distance"]
-		beam1.rssi = data["transducers"][1]["rssi"]
-		beam1.nsd = data["transducers"][1]["nsd"]
-		beam1.valid = data["transducers"][1]["beam_valid"]
+			beam1.id = data["transducers"][1]["id"]
+			beam1.velocity = data["transducers"][1]["velocity"]
+			beam1.distance = data["transducers"][1]["distance"]
+			beam1.rssi = data["transducers"][1]["rssi"]
+			beam1.nsd = data["transducers"][1]["nsd"]
+			beam1.valid = data["transducers"][1]["beam_valid"]
 		
-		beam2.id = data["transducers"][2]["id"]
-		beam2.velocity = data["transducers"][2]["velocity"]
-		beam2.distance = data["transducers"][2]["distance"]
-		beam2.rssi = data["transducers"][2]["rssi"]
-		beam2.nsd = data["transducers"][2]["nsd"]
-		beam2.valid = data["transducers"][2]["beam_valid"]
+			beam2.id = data["transducers"][2]["id"]
+			beam2.velocity = data["transducers"][2]["velocity"]
+			beam2.distance = data["transducers"][2]["distance"]
+			beam2.rssi = data["transducers"][2]["rssi"]
+			beam2.nsd = data["transducers"][2]["nsd"]
+			beam2.valid = data["transducers"][2]["beam_valid"]
 		
-		beam3.id = data["transducers"][3]["id"]
-		beam3.velocity = data["transducers"][3]["velocity"]
-		beam3.distance = data["transducers"][3]["distance"]
-		beam3.rssi = data["transducers"][3]["rssi"]
-		beam3.nsd = data["transducers"][3]["nsd"]
-		beam3.valid = data["transducers"][3]["beam_valid"]
+			beam3.id = data["transducers"][3]["id"]
+			beam3.velocity = data["transducers"][3]["velocity"]
+			beam3.distance = data["transducers"][3]["distance"]
+			beam3.rssi = data["transducers"][3]["rssi"]
+			beam3.nsd = data["transducers"][3]["nsd"]
+			beam3.valid = data["transducers"][3]["beam_valid"]
 		
-		theDVL.beams = [beam0, beam1, beam2, beam3]
+			theDVL.beams = [beam0, beam1, beam2, beam3]
 		
-		pub.publish(theDVL)
+			pub.publish(theDVL)
 		
 		rate.sleep()
 
